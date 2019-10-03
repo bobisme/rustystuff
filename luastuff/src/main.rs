3ab21518f0f0fcf875b650aead5418f668453fbf
Bob <shiftsource@gmail.com>
2019-10-03 10:22:07 -0700

    BROKEN: ffi threading problems

--- luastuff/src/main.rs
+++ luastuff/src/main.rs
@@ -1,7 +1,8 @@
 extern crate rustyline;
+use std::sync::{Arc, Mutex};
 use std::convert::From;
 
-use rlua::{Lua};
+use rlua::{Lua, Function, Integer};
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
@@ -18,6 +19,35 @@ impl From<ReadlineError> for Error {
     fn from(err: ReadlineError) -> Error { Error::Readline(err) }
 }
 
+fn setup(lua: Lua) -> Result<(), Error> {
+    lua.context(|lua_ctx| {
+        let globals_arc = Arc::new(Mutex::new(lua_ctx.globals()));
+
+        let sum = lua_ctx.create_function(|_, list: (Vec<i32>)| {
+            let res: i32 = list.iter().sum();
+            Ok(res)
+        })?;
+        {
+            let globals = globals_arc.lock().unwrap();
+            globals.set("sum", sum)?;
+        }
+
+        let print_sum = lua_ctx.create_function(|_, (list, label): (Vec<i32>, String)| {
+            let globals = globals_arc.lock().unwrap();
+            // let print: Function = globals.get("print")?;
+            // let res = sum.call::<_, Integer>(list)?;
+            let res = 42i32;
+            // print.call::<_, ()>(format!("{}: {}", label, res))?;
+            Ok(())
+        })?;
+        {
+            let globals = globals_arc.lock().unwrap();
+            globals.set("print_sum", print_sum)?;
+        }
+        Ok(())
+    })
+}
+
 fn main() -> Result<(), Error> {
     let lua = Lua::new();
 
@@ -25,12 +55,15 @@ fn main() -> Result<(), Error> {
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
     }
-    lua.context(|lua_ctx| {
-        loop {
-            let readline = rl.readline(">> ");
-            match readline {
-                Ok(line) => {
-                    rl.add_history_entry(line.as_str());
+
+    setup(lua)?;
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                lua.context(|lua_ctx| {
                     match lua_ctx.load(line.as_str()).exec() {
                         Ok(_) => {},
                         Err(err @ rlua::Error::SyntaxError { .. }) => {
@@ -38,13 +71,13 @@ fn main() -> Result<(), Error> {
                         },
                         Err(err) => println!("Error: {:?}", err),
                     }
-                },
-                Err(ReadlineError::Interrupted) => { break },
-                Err(ReadlineError::Eof) => { break },
-                Err(err) => { println!("Error: {:?}", err) }
-            }
+                })
+            },
+            Err(ReadlineError::Interrupted) => { break },
+            Err(ReadlineError::Eof) => { break },
+            Err(err) => { println!("Error: {:?}", err) }
         }
-        rl.save_history("history.txt")?;
-        Ok(())
-    })
+    }
+    rl.save_history("history.txt")?;
+    Ok(())
 }
